98a261e27b6bb9a096313efd57acfb52304a4371
eduwass@gmail.com <eduwass@gmail.com@04a00154-add3-25d7-fcb8-b155e40d896b>
2014-03-09 12:43:05 +0000

    - Opciones 4,5 del menú funcionales para ReproductorUB1A

--- WassRosadoEduardo/src/edu/ub/prog2/WassRosadoEduardo/vista/ReproductorUB1A.java
+++ WassRosadoEduardo/src/edu/ub/prog2/WassRosadoEduardo/vista/ReproductorUB1A.java
@@ -8,6 +8,12 @@ package edu.ub.prog2.WassRosadoEduardo.vista;
 import edu.ub.prog2.WassRosadoEduardo.model.FitxerAudio;
 import edu.ub.prog2.WassRosadoEduardo.model.TaulaFitxers;
 import edu.ub.prog2.utils.Menu;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 /**
@@ -23,15 +29,20 @@ public class ReproductorUB1A {
     public TaulaFitxers taula;
     
     // Declarem les opcions per a referir-se a les opcions del menú.
-    static private enum OpcionsMenuPrincipal {MENU_PRINCIPAL_OPCIO1,MENU_PRINCIPAL_OPCIO2,MENU_PRINCIPAL_OPCIO3,MENU_PRINCIPAL_OPCIO4,MENU_PRINCIPAL_OPCIO5,MENU_PRINCIPAL_SORTIR};
+    static private enum OpcionsMenuPrincipal {  MENU_PRINCIPAL_OPCIO1,
+                                                MENU_PRINCIPAL_OPCIO2,
+                                                MENU_PRINCIPAL_OPCIO3,
+                                                MENU_PRINCIPAL_OPCIO4,
+                                                MENU_PRINCIPAL_OPCIO5,
+                                                MENU_PRINCIPAL_SORTIR};
     
     // Declarem descripcions personalitzades per a les opcions del menú principal
-    private static final String[] descMenuPrincipal={   "Afegir fitxer",
-                                                        "Mostrar llista",
-                                                        "Eliminar fitxer",
-                                                        "Guardar llista",
-                                                        "Recuperar llista",
-                                                        "Sortir de l'aplicació"};
+    private static final String[] descMenuPrincipal={"Afegir fitxer",
+                                                    "Mostrar llista",
+                                                    "Eliminar fitxer",
+                                                    "Guardar llista",
+                                                    "Recuperar llista",
+                                                    "Sortir de l'aplicació"};
 
     // ========================
     // Funciones
@@ -66,7 +77,8 @@ public class ReproductorUB1A {
      */
     private void gestioMenuPrincipal(Scanner sc) {
 
-        // Creem l'objecte per al menú. Li passem com a primer paràmetre el nom del menú
+        // Creem l'objecte per al menú. 
+        // Li passem com a primer paràmetre el nom del menú
         Menu menu=new Menu("Menu Principal",OpcionsMenuPrincipal.values());
 
         // Assignem la descripció de les opcions
@@ -101,13 +113,25 @@ public class ReproductorUB1A {
                 case MENU_PRINCIPAL_OPCIO4:
                     // Mostrem un missatge indicant que s'ha triat aquesta opció
                     System.out.println("Has triat la opció 4");
-                    guardarLlista(sc);
+                    try {
+                        guardarLlista(sc);
+                    } catch (IOException ex) {
+                        System.out.println("Error d'escritura.");
+                    }
                     break;
                 case MENU_PRINCIPAL_OPCIO5:
                     // Mostrem un missatge indicant que s'ha triat aquesta opció
                     System.out.println("Has triat la opció 5");
-                    recuperarLlista(sc);
-                    break;      
+                    try {
+                        recuperarLlista(sc);
+                    } catch (FileNotFoundException ex) {
+                        System.out.println("L'arxiu no s'ha trobat.");
+                    } catch (IOException ex) {
+                        System.out.println("Error de lectura.");
+                    } catch (ClassNotFoundException ex) {
+                        System.out.println("Classe no trobata.");
+                    }
+                    break;                        
                 case MENU_PRINCIPAL_SORTIR:
                     System.out.println("Fins aviat!");
                     break;
@@ -146,34 +170,52 @@ public class ReproductorUB1A {
      * @param sc scanner
      */
     private void eliminarFitxer(Scanner sc){
-        // Donat un nom, cercar i retornar FitxerAudio
+        // Donat una ruta, cercar a taula i retornar FitxerAudio
         System.out.println("Dona la ruta del fitxer a eliminar:");
-        // Demanar dades de fitxer per teclat
+        // Demanar dades de ruta per teclat
         String nomFitxer = sc.nextLine();
-        // Pasar FitxerAudio a taula.eliminarFitxer
+        // Cercar FitxerAudio i si ho troba passar a taula.eliminarFitxer
         for(int i=0;i<this.taula.tamany();i++){
             FitxerAudio Actual = this.taula.getAt(i);
             if(nomFitxer.equals(Actual.getPath())){
                 // Si path coincideix
                 this.taula.eliminarFitxer(Actual);
+                System.out.println("Fitxer eliminat.");
+                return; // finalitzar
             }
         }
+        // Si no l'ha trobat:
+        System.out.println("No s'ha trobat cap fitxer amb aquesta ruta.");
     }
     
     /**
      * Pas 4 del menu: guardar llista a fitxer
      * @param sc scanner
      */
-    private void guardarLlista(Scanner sc){
-        
+    private void guardarLlista(Scanner sc) throws IOException{
+        // Demana ruta per guardar
+        System.out.println("Dona la ruta per guardar llista:");
+        // Demanar dades de ruta per teclat
+        String nomFitxer = sc.nextLine();
+        FileOutputStream fout = new FileOutputStream(nomFitxer);
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(this.taula);
+        fout.close();
     }
     
     /**
      * Pas 5 del menu: carregar llista desde fitxer
      * @param sc  scanner
      */
-    private void recuperarLlista(Scanner sc){
-        
+    private void recuperarLlista(Scanner sc) throws FileNotFoundException, IOException, ClassNotFoundException{
+        // Demana ruta per guardar
+        System.out.println("Dona la ruta de l'arxiu per recuperar les dades:");
+        // Demanar dades de ruta per teclat
+        String nomFitxer = sc.nextLine();
+        FileInputStream fin = new FileInputStream(nomFitxer);
+        ObjectInputStream ois = new ObjectInputStream(fin);
+        this.taula = (TaulaFitxers) ois.readObject();
+        fin.close();        
     }
     
    
