817b7470929f00a3f1fead3740cfe653c8015b49
eduwass@gmail.com <eduwass@gmail.com@04a00154-add3-25d7-fcb8-b155e40d896b>
2014-03-19 08:51:49 +0000

    - Creada estructura inicial para la Practica2

---
+++ WassRosadoEduardo2/src/edu/ub/prog2/WassRosadoEduardo/vista/ReproductorUB2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.ub.prog2.WassRosadoEduardo.vista;
+
+/**
+ *
+ * @author mat.aules
+ */
+public class ReproductorUB2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
